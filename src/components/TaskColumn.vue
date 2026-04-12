dc434bd37c02ca8ec7163abc3f5cc93315286638
natali7109 <you@example.com>
2026-04-12 15:48:08 +0300

    Компоненты готовы. Задачи сделаны

---
+++ src/components/TaskColumn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main__column">
+    <div class="column__title">
+      <p>{{ title }}</p>
+    </div>
+    <div class="cards">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskColumn',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/components/TaskDesk.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main">
+    <div class="container">
+      <div class="main__block">
+        <div class="main__content">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'TaskDesk'
+}
+</script>
